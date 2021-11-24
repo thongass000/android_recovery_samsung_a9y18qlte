fdea0b2a5df16977ec8cf1cf33dd4284670870e0
thongass000 <tienthong2004@gmail.com>
2021-11-24 20:02:09 +0700

    Initial commit for SHRP device tree

---
+++ README.md
@@ -0,0 +1,9 @@
+# Samsung Galaxy A9 (2018) SM-A920F device tree for TWRP
+twrp_android_device_samsung_a9y18qlte
+
+# TWRP-9.0
+Manifest used
+https://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni/tree/twrp-9.0
+
+Kernel source:
+https://github.com/thongass000/twrp_android_kernel_samsung_a9y18qlte
---
+++ omni_a9y18qlte.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 The TWRP Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from a9y18qlte device
+$(call inherit-product, device/samsung/a70q/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := a9y18qlte
+PRODUCT_NAME := omni_a9y18qlte
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-A920F
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := a9y18qlte
