22f31eb116ab8c2b2c1fb0b9b0c4469fec33b576
thongass000 <tienthong2004@gmail.com>
2022-04-22 12:32:14 +0700

    Add script to obtain latest Magisk Stable

---
+++ magisk.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+stable=https://raw.githubusercontent.com/topjohnwu/magisk-files/master/stable.json
+stable_ver=$(curl -s $stable | sed -n 3p | cut -d ":" -f 2)
+stable_link=$(curl -s $stable | sed -n 5p | sed 's/ //g' | cut -d : -f 2,3)
+ver=${stable_ver:2:-2}
+link=${stable_link:1:-2}
+
+if [[ $RECOVERY == "OrangeFox" ]]; then
+	l=recovery/FoxFiles
+	f=Magisk.zip
+elif [[ $RECOVERY == "SHRP" ]]; then
+	l=shrp/extras
+	f=c_magisk.zip
+fi
+
+echo "- Latest Magisk Stable release is $ver"
+echo "- Link: $link"
+sleep 1
+echo "- Getting latest Magisk Stable ..."
+cd vendor/$l
+wget -O $f $link
+echo "- Done!"
