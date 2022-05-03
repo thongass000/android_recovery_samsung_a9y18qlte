e553c90f684d3de2f2be0de1fd681ce7237b9e87
James Nguyen <tienthong2004@gmail.com>
2022-05-03 15:52:36 +0700

    Update magisk.sh

--- magisk.sh
+++ magisk.sh
@@ -5,10 +5,10 @@ stable_link=$(curl -s $stable | sed -n 5p | sed 's/ //g' | cut -d : -f 2,3)
 ver=${stable_ver:2:-2}
 link=${stable_link:1:-2}
 
-if [[ $RECOVERY == "OrangeFox" ]]; then
+if [[ $1 == "OrangeFox" ]]; then
 	l=recovery/FoxFiles
 	f=Magisk.zip
-elif [[ $RECOVERY == "SHRP" ]]; then
+elif [[ $1 == "SHRP" ]]; then
 	l=shrp/extras
 	f=c_magisk.zip
 fi
